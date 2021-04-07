3854f990eb5e1fdef88425e7589f37cf294aca05
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-04-08 00:28:46 +0200

    infinit scroll functionality

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
-let photosArray = [];
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
 function setAttributes(element, attributes) {
   for (const key in attributes)
   {
     element.setAttribute(key, attributes[key]);
   }
 }
-
 // create elements for links and photos, add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
     setAttributes(item, {
@@ -18,17 +23,15 @@ function displayPhotos() {
       target: '_blank',
     });
     const img = document.createElement('img');
-
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     });
-
+    // event listner
+    img.addEventListener('load', imageLoaded)
     item.appendChild(img);
     imageContainer.appendChild(item);
-
-
   });
 }
 // unsplash API 
@@ -37,9 +40,22 @@ const apiKey = 'R5drk7TP-JoRViTt39M18ehNeSg2O_LzEe1WvxT5Jcs';
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// check if images loaded
+
+
+function imageLoaded() {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages)
+  {
+    ready = true;
+    loader.hidden = true;
+    count = 30;
+
+  }
+}
+
 // get photos
-
-
 async function getPhotos() {
   try
   {
@@ -48,7 +64,18 @@ async function getPhotos() {
     displayPhotos();
   } catch (error)
   {
-
   }
 }
+// load more photos event listner
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready)
+  {
+    ready = false;
+    getPhotos();
+
+  }
+});
+
+
+
 getPhotos();
